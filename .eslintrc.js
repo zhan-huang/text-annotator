d5d45f2bc3622c78eea94396b66e1f01604c0285
Zhan Huang <zhuang@ebi.ac.uk>
2020-11-04 15:13:27 +0000

    rewrite text-annotator in TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": 0
   }
 };
